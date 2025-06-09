00157d8cc846b5c620e3a0715cfbd390bddd1c5f
DevRyujin <santosdrei24@gmail.com>
2025-06-09 18:45:08 +0800

    Initial commit

---
+++ frontend-mealsonwheels/src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div style={{ background: "#eee", padding: "10px" }}>Header</div>;
+}
---
+++ frontend-mealsonwheels/src/components/Navbar.jsx
@@ -0,0 +1,3 @@
+export default function Navbar() {
+  return <div style={{ background: "#ddd", padding: "10px" }}>Navbar</div>;
+}
