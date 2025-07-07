be49096bfee262fd643e48e17c96539a489d645d
DevRyujin <santosdrei24@gmail.com>
2025-07-07 21:02:59 +0800

    Donation pages

--- frontend-mealsonwheels/src/App.jsx
+++ frontend-mealsonwheels/src/App.jsx
@@ -48,10 +48,10 @@ import Contact from './pages/Contact';
 import Home from './pages/Home.jsx';
 
 // Donation
-import DonateAmountPage from './Donation/DonateAmountPage.jsx';
-import BillingDetails from './Donation/BillingDetails.jsx';
-import DonatePayment from './Donation/DonatePayment.jsx'
-import DonateComplete from './Donation/DonationCompletion.jsx'
+import DonateAmountPage from './pages/Donation/DonateAmountPage.jsx';
+import BillingDetails from './pages/Donation/BillingDetails.jsx';
+import DonatePayment from './pages/Donation/DonatePayment.jsx'
+import DonateComplete from './pages/Donation/DonationCompletion.jsx'
 
 // Share Pages
 import RedirectFoodSafety from './pages/Shared/RedirectFoodSafety';
--- frontend-mealsonwheels/src/pages/Donation/BillingDetails.jsx
+++ frontend-mealsonwheels/src/pages/Donation/BillingDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axiosInstance from '../api/axiosInstance';
+import axiosInstance from '../../api/axiosInstance';
 
 export default function BillingDetails() {
   const navigate = useNavigate();
